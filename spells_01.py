df570e8b75fdb307201de5af5c4d10e8ce9427e0
SJBSR <sjb.sr@outlook.com>
2025-11-19 13:52:54 -0500

    inset_lambda

--- spells_01.py
+++ spells_01.py
@@ -5,18 +5,21 @@ spells = [# (Spell, Damange, Energy)
     ("Ice Spike", 40,30),
     ("Lightning Strike", 60,50),
     ("Wind Gust", 20, 10),
-    ("Earthquake", 60, 60)
-    ("Water Blast", 30, 20)
+    ("Earthquake", 60, 60),
+    ("Water Blast", 30, 20),
     ("Dark Matter", 70, 80)]
 
 def dmg_per_energy(s):
     # handle zero energy to avoid ZeroDivisionError
     return s[1] / s[2] if s[2] != 0 else float('inf')
 
-
+# Used lamba function to sort spells based on efficiency (damage per energy)
+# Lambda function is an anonymous function defined with the lambda keyword
+# Orginal sorting code commented out
 efficient_spells = sorted(
         spells,
-        key = dmg_per_energy,
+        # key = dmg_per_energy,
+        key = lambda s: dmg_per_energy(s),
         reverse = True)
 
 for s in efficient_spells:
